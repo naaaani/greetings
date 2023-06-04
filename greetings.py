beb33c6518f8fd361cb7bf65be8bdf62cb63c4d5
naaaani <nandi.zalka@gmail.com>
2023-06-05 00:48:21 +0200

    Add Incomplete todaysword()

--- greetings.py
+++ greetings.py
@@ -2,7 +2,7 @@
 import random
 import os
 
-dirty_words = open("/home/nandi/petProject/greetings/dirty_words.txt", "r")
+dirty_words = open('/home/nandi/petProject/greetings/dirty_words.txt', 'r')
 content = dirty_words.readlines()
 dirty_words.close()
 
@@ -10,12 +10,22 @@ def getRandomLine(lines):
     line = lines[random.randint(0, 355)].strip()
     return line
 
-def createMessage(title, message):
-    return "notify-send" + " " + title + " " +  message
+def todaysWord():
+    
+    if os.stat('/home/nandi/petProject/greetings/today.txt').st_size == 0:
+        today = open('/home/nandi/petProject/greetings/today.txt', 'r+')
+        today.write(getRandomLine(content))
+        today.close()
+
+    today = open('/home/nandi/petProject/greetings/today.txt', 'r')
+    word = today.readline()
+    today.close()
+    return word
 
 def createAlert():
-    line = getRandomLine(content)
+    word = todaysWord()
 
-    os.system("notify-send 'Üdvözöllek, " + line + "' 'Ideje dolgozni'")
+    os.system("notify-send 'Üdvözöllek, " + word + "' 'Ideje dolgozni'")
+    
     
 createAlert()
