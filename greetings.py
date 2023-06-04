e67f921879973fcceddb98b852128f12aaea899a
naaaani <nandi.zalka@gmail.com>
2023-06-04 21:16:16 +0200

    Send message with random line

---
+++ greetings.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import random
+import os
+
+dirty_words = open("/home/nandi/petProject/dirty_words.txt", "r")
+content = dirty_words.readlines()
+dirty_words.close()
+
+def getRandomLine(lines):
+    line = lines[random.randint(0, 355)].strip()
+    return line
+
+def createMessage(title, message):
+    return "notify-send" + " " + title + " " +  message
+
+def createAlert():
+    line = getRandomLine(content)
+
+    os.system("notify-send 'Üdvözöllek, " + line + "' 'Ideje dolgozni'")
+    
+
+createAlert()
