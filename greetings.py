c3fe8246aa215e3be40b058e73669227c2272607
naaaani <nandi.zalka@gmail.com>
2023-06-05 15:55:47 +0200

    Add getDate and check_date

--- greetings.py
+++ greetings.py
@@ -1,15 +1,35 @@
 #!/usr/bin/env python3
 import random
 import os
+import datetime
 
 dirty_words = open('/home/nandi/petProject/greetings/dirty_words.txt', 'r')
 content = dirty_words.readlines()
 dirty_words.close()
 
+def getDate():
+    current = datetime.datetime.now()
+    return [current.year, current.month, current.day]
+
+today = {
+    'date': [],
+    'word': 'fasz',
+    'used': [],
+}
+
+def check_date(today):
+    date = datetime.datetime.now()
+    todays_date = [date.year, date.month, date.day]
+
+    if todays_date != today['date']:
+        today['date'] = todays_date
+        print(today)
+    
 def getRandomLine(lines):
     line = lines[random.randint(0, 355)].strip()
     return line
 
+#TODO implement json solution
 def todaysWord():
     
     if os.stat('/home/nandi/petProject/greetings/today.txt').st_size == 0:
@@ -29,3 +49,4 @@ def createAlert():
     
     
 createAlert()
+check_date(today)
