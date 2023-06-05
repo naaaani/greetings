dfe58d40c6ced8ad5ee80c0bd932fcea11334789
naaaani <nandi.zalka@gmail.com>
2023-06-05 16:26:35 +0200

    Implement Save Date With JSON

--- greetings.py
+++ greetings.py
@@ -2,34 +2,42 @@
 import random
 import os
 import datetime
+import json
 
 dirty_words = open('/home/nandi/petProject/greetings/dirty_words.txt', 'r')
 content = dirty_words.readlines()
 dirty_words.close()
 
+def get_data(filename):
+    with open(filename, 'r') as file:
+        content = json.load(file)
+        return content
+
 def getDate():
     current = datetime.datetime.now()
     return [current.year, current.month, current.day]
 
-today = {
-    'date': [],
-    'word': 'fasz',
-    'used': [],
-}
-
 def check_date(today):
     date = datetime.datetime.now()
     todays_date = [date.year, date.month, date.day]
+    data = get_data('data.json')
+    stored_date = data['date']
 
-    if todays_date != today['date']:
-        today['date'] = todays_date
-        print(today)
-    
+    if todays_date != stored_date:
+        data['date'] = todays_date
+        set_new_date(data)
+
+
+def set_new_date(update):
+    with open('data.json', 'w') as data:
+        json.dump(update, data)
+
+
+
 def getRandomLine(lines):
     line = lines[random.randint(0, 355)].strip()
     return line
 
-#TODO implement json solution
 def todaysWord():
     
     if os.stat('/home/nandi/petProject/greetings/today.txt').st_size == 0:
